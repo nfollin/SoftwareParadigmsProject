a75692855c95496eba92270b25b067b3dbdd2be9
Nathan Follin <Follin_Nathan@bah.com>
2014-06-05 22:21:54 -0400

    start of anfroid app

---
+++ Photon/src/team/Photon/Photon.java
@@ -0,0 +1,15 @@
+package team.Photon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Photon extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
